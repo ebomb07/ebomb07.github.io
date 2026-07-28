868c66bc2596bd593d24d26ee4af693e85f39a03
David Macpherson <david.allan.macpherson@gmail.com>
2026-07-28 15:08:57 -0400

    bash script

---
+++ push.sh
@@ -0,0 +1,17 @@
+## insert backdoor here - david
+
+mkdocs build
+
+git add *
+git commit -m "new writeup"
+git push main
+
+cd site
+git add *
+git commit -m "new writeup for site"
+git push master
+
+
+
+
+
