ecadd65ab95491ac0b24b0379bd31f5af15f293a
David Macpherson <david.allan.macpherson@gmail.com>
2026-07-28 15:10:17 -0400

    changes

--- push.sh
+++ push.sh
@@ -7,8 +7,10 @@ git commit -m "new writeup"
 git push main
 
 cd site
+git init
 git add *
 git commit -m "new writeup for site"
+git remote add origin ebomb07/ebomb07.github.io
 git push master
 
 
